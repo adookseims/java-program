d4b9c58f434e70745cf0736154d1b3d29ce655d0
Adrian Smiesko <adrian.smiesko@gmail.com>
2024-07-08 10:41:47 +0200

    add counter

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -2,7 +2,11 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int a = 5;
+        for (int i = 0; i <=a;i++) {
+            System.out.print(i);
 
+        }
     }
 }
 
